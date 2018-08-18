c19a46af479e94d878304b7218d226cc609c4c17
clinton wambugu <30356189+kwambugu@users.noreply.github.com>
2018-08-18 09:22:22 +0300

    uploaded index.html ,main.css and main.js

---
+++ main.js
@@ -0,0 +1,22 @@
+ var content = document.querySelector('.content');
+
+ for( var i = 0; i <= 14; i++){
+     //width
+     for (let j = 0; j <= 16; j++) {
+         //height
+         var div = document.createElement("div");
+         div.style.width = "24.9px";
+         div.style.height = "24.9px";
+         div.style.border ="0.1px solid #9e9e9e"; 
+         content.appendChild(div);
+         div.classList.add('pixel');
+     }
+ }
+
+ function pixelate(){
+      var pixel = document.querySelector('.pixel')
+      pixel.setAttribute('class', 'pixelated');
+ }
+ 
+ var allPixels = document.querySelectorAll('div');
+ allPixels.forEach((e)=> e.addEventListener('mouseleave',pixelate));
